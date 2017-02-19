abce89c41201c950acaf8abb741394d4cda4eb75
asu <arsenyk.ustaris.dev@gmail.com>
2017-02-19 13:56:04 -0800

    Initial

---
+++ w1/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,35 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(c: Int, r: Int): Int =
+    {
+      return 1
+    }
+
+    def pascalIter(c:Int, r:Int, previous:List[Char]): List[Char] =
+    {
+      return
+    }
+  
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = ???
+  
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = ???
+  }
