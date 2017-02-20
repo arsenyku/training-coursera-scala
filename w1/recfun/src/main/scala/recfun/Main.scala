5317cb1965d51ceda8285dca591b92a3ae7d6c84
asu <arsenyk.ustaris.dev@gmail.com>
2017-02-20 01:03:47 -0800

    Style changes per Coursera style check: - No mutable local variables (including ListBuffer) - Eliminate all early returns - Replace return statements with function result

--- w1/recfun/src/main/scala/recfun/Main.scala
+++ w1/recfun/src/main/scala/recfun/Main.scala
@@ -1,7 +1,5 @@
 package recfun
 
-import scala.collection.mutable.ListBuffer
-
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -18,32 +16,22 @@ object Main {
   def pascal(c: Int, r: Int): Int =
   {
     val row = pascalIter(List(), r + 1)
-    return row(c)
+    row(c)
   }
 
   def pascalIter(previous: List[Int], maxLength: Int): List[Int] =
   {
-    if (previous.size >= maxLength)
+    if (previous.size >= maxLength) previous
+    else
     {
-      return previous
+      val row = for (i <- 0 to previous.size - 1) yield
+      {
+        val left = if (i == 0) 0 else previous(i-1)
+        val right = previous(i)
+        left + right
+      }
+      pascalIter(row.toList ++ List(1), maxLength)
     }
-
-    var row = ListBuffer[Int]()
-    var left: Int = 0
-    var right: Int = 0
-    for (i <- 0 to previous.size - 1)
-    {
-      right = previous(i)
-
-      row += left + right
-
-      left = right
-    }
-
-    row += 1
-
-    return pascalIter(row.toList, maxLength)
-
   }
 
   /**
@@ -51,30 +39,28 @@ object Main {
     */
   def balance(chars: List[Char]): Boolean =
   {
-    return balanceIter(0, chars) == 0
+    balanceIter(0, chars) == 0
   }
 
   def balanceIter(balanceValue: Int, chars: List[Char]): Int =
   {
     if (chars.size < 1 || balanceValue < 0)
     {
-      return balanceValue
+      balanceValue
     }
-
-    val first = chars(0)
-    val rest = chars.drop(1)
-    var newBalance = balanceValue
-
-    if (first == '(')
+    else
     {
-      newBalance = balanceValue + 1
-    }
-    else if (first == ')')
-    {
-      newBalance = balanceValue - 1
-    }
+      val first = chars(0)
+      val rest = chars.drop(1)
 
-    return balanceIter(newBalance, rest)
+      val newBalance =
+        if (first == '(') balanceValue + 1
+        else if (first == ')') balanceValue - 1
+        else balanceValue
+
+      balanceIter(newBalance, rest)
+
+    }
   }
 
 
@@ -83,37 +69,34 @@ object Main {
     */
   def countChange(money: Int, coins: List[Int]): Int =
   {
-    val answer = countChangeIter(0, money, money, coins.sortWith((x, y) => x > y))
-    return answer
+    countChangeIter(0, money, money, coins.sortWith((x, y) => x > y))
   }
 
   def countChangeIter(waysToCount: Int, remainingChange: Int, originalMoney: Int, coins: List[Int]): Int =
   {
     if (coins.isEmpty)
     {
-      return waysToCount
+      waysToCount
     }
-
-    val head = coins.head
-    val tail = coins.tail
-
-    val newRemainingChange = remainingChange - head
-
-    var result = waysToCount
-
-    if (newRemainingChange < 0)
+    else
     {
-      result += countChangeIter(waysToCount, remainingChange, originalMoney, tail)
-    }
-    else //if (newRemainingChange > 0)
-    {
-      if (newRemainingChange == 0) { result += 1 }
+      val head = coins.head
+      val tail = coins.tail
 
-      result += countChangeIter(waysToCount, newRemainingChange, originalMoney, coins) +
-        countChangeIter(waysToCount, remainingChange, originalMoney, tail)
-    }
+      val newRemainingChange = remainingChange - head
 
-    return result
+      if (newRemainingChange < 0)
+      {
+        waysToCount + countChangeIter(waysToCount, remainingChange, originalMoney, tail)
+      }
+      else //if (newRemainingChange > 0)
+      {
+        val foundMatch = if (newRemainingChange == 0) 1 else 0
+
+        foundMatch + countChangeIter(waysToCount, newRemainingChange, originalMoney, coins) +
+          countChangeIter(waysToCount, remainingChange, originalMoney, tail)
+      }
+    }
 
   }
 
